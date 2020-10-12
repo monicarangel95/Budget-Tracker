e1bcb7a6ea16db12b37a939dde42b573a11e1117
Monica Rangel <monicarangel95@github.com>
2020-10-11 21:26:54 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-
 const PORT = 3000;
-
 const app = express();
-
 app.use(logger("dev"));
-
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+//Adding my db connection here//
+mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
 });
 
 // routes
 app.use(require("./routes/api.js"));
-
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
