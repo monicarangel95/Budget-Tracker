12bcfc6c6a10a9ce2aa5844668b48bce3ac71bf8
Monica Rangel <monicarangel95@github.com>
2020-10-11 21:31:08 -0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
 
-router.post("/api/transaction", ({body}, res) => {
+router.post("/api/transaction", ({ body }, res) => {
   Transaction.create(body)
     .then(dbTransaction => {
       res.json(dbTransaction);
@@ -10,8 +10,7 @@ router.post("/api/transaction", ({body}, res) => {
       res.status(404).json(err);
     });
 });
-
-router.post("/api/transaction/bulk", ({body}, res) => {
+router.post("/api/transaction/bulk", ({ body }, res) => {
   Transaction.insertMany(body)
     .then(dbTransaction => {
       res.json(dbTransaction);
@@ -20,9 +19,8 @@ router.post("/api/transaction/bulk", ({body}, res) => {
       res.status(404).json(err);
     });
 });
-
 router.get("/api/transaction", (req, res) => {
-  Transaction.find({}).sort({date: -1})
+  Transaction.find({}).sort({ date: -1 })
     .then(dbTransaction => {
       res.json(dbTransaction);
     })
@@ -30,5 +28,4 @@ router.get("/api/transaction", (req, res) => {
       res.status(404).json(err);
     });
 });
-
 module.exports = router;
